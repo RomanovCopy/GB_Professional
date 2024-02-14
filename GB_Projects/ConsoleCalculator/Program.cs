d5793416792b595adf1dabb66c86bf29606adf15
Сергей Романов <RomanovCopy@gmail.com>
2024-02-14 11:11:03 +0300

    fix

---
+++ GB_Projects/ConsoleCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private static double[] ParserStringToInt(String[] rows, int size)
+        {
+            double number = 0;
+            double[] numbers = new double[size];
+            foreach(var row in rows)
+            {
+                if(double)
+            }
+
+            return numbers;
+        }
+
+        private 
+
+        
+    }
+}
