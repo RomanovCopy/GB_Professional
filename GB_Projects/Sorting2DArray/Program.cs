797a6707582e333367356a3a025d4c5130c9d264
RomanovCopy <RomanovCopy@gmail.com>
2024-02-15 22:33:50 +0300

    fix1502242233

---
+++ GB_Projects/Sorting2DArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting2DArray
+{
+    /*
+     * Дан двумерный массив.
+
+    732
+    496
+    185
+
+    Отсортировать данные в нем по возрастанию.
+
+    123
+    456
+    789
+
+    Вывести результат на печать.
+
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static int[,] Sorting2DArray(int[,] array)
+        {
+            int rows = array.GetLength(0);
+            int coils = array.GetLength(1);
+        }
+    }
+}
