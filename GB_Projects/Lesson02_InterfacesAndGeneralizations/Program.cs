914cc3000391c181a98e8aca8f50aa5151846dd6
RomanovCopy <RomanovCopy@gmail.com>
2024-02-23 12:31:44 +0300

    Start

---
+++ GB_Projects/Lesson02_InterfacesAndGeneralizations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02_InterfacesAndGeneralizations
+{
+
+    /* Реализуйте операторы неявного приведения из long, int, byt в Bits. */
+    internal class Program
+    {
+        static void Main ( string[ ] args )
+        {
+        }
+    }
+}
