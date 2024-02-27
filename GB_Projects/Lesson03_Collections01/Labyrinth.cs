8e89ae796ffc18429a49f3fcb757825c26d7a178
Сергей Романов <RomanovCopy@gmail.com>
2024-02-27 15:27:17 +0300

    fix2702241527

---
+++ GB_Projects/Lesson03_Collections01/Labyrinth.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03_Collections01
+{
+    internal class Labyrinth
+    {
+        private Stack<Tuple<int , int>> currentPath = new Stack<Tuple<int , int>>( );
+        private List<Stack<Tuple<int , int>>> allPaths = new List<Stack<Tuple<int , int>>>( );
+        private readonly int columns;
+        private readonly int rows;
+        private int steps = 0;
+
+        public Labyrinth ( int[ , ] labirinth , Tuple<int , int> startPoint )
+        {
+            rows = labirinth.GetLength( 0 );
+            columns = labirinth.GetLength( 1 );
+
+
+            Console.WriteLine( $" Найдено {HasExit( startPoint.Item1 , startPoint.Item2 , labirinth )} выходов из лабиринта \n" );
+            steps = 1;
+
+            foreach ( var p in allPaths )
+            {
+                Console.Write( $"{steps}  " );
+                while ( p.Count > 0 )
+                {
+                    var item = p.Pop( );
+                    Console.Write( $"({item.Item1}, {item.Item2}) -> " );
+                }
+                Console.WriteLine( );
+                steps++;
+            }
+        }
+
+
+        private int HasExit ( int startI , int startJ , int[ , ] l )
+        {
+            int row = startI;
+            int column = startJ;
+            if ( row >= 0 && row < rows && column >= 0 && column < columns && l[ row , column ] == 0 )
+            {
+                Validate( row , column , l );
+            }
+            return allPaths.Count;
+        }
+
+        private void Validate(int row, int column, int[ , ]l)
+        {
+            if ( l[row,column]==0 )
+            {
+                if(row!=rows-1 && column != columns - 1 && row >= 0 && column >= 0)
+                {
+                    l[ row , column ] = 1;
+                    currentPath.Push( new Tuple<int , int>( row , column ) );
+                    Validate( row + 1 , column , l );
+                    if ( row > 0 )
+                        Validate( row - 1 , column , l );
+                    Validate( row , column + 1 , l );
+                    if ( column > 0 )
+                        Validate( row , column - 1 , l );
+                    l[ row , column ] = 0;
+                    currentPath.Pop( );
+                } else
+                {
+                    currentPath.Push( new Tuple<int , int>( row , column ) );
+                    allPaths.Add( new Stack<Tuple<int, int>>(currentPath) );
+                }
+            }
+            return;
+        }
+
+    }
+}
---
+++ GB_Projects/Lesson03_Collections01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson03_Collections01
+{
+    internal class Program
+    {
+        private static int[ , ] labirynth1 = new int[ , ]
+        {
+            {1, 1, 1, 1, 1, 1, 1 },
+            {1, 0, 0, 0, 0, 0, 1 },
+            {1, 0, 1, 1, 1, 0, 1 },
+            {0, 0, 0, 0, 1, 0, 0 },
+            {1, 1, 0, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 }
+        };
+
+
+        static void Main ( string[ ] args )
+        {
+            new Labyrinth( labirynth1 ,  new Tuple<int , int>( 3 , 0 ));
+        }
+    }
+}
