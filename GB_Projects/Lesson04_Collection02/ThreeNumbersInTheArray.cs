999ac24946f2054b6afa55a052b730279077c3f6
Сергей Романов <RomanovCopy@gmail.com>
2024-02-29 13:42:42 +0300

    CompleteV01

--- GB_Projects/Lesson04_Collection02/ThreeNumbersInTheArray.cs
+++ GB_Projects/Lesson04_Collection02/ThreeNumbersInTheArray.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -25,32 +26,51 @@ namespace Lesson04_Collection02
             21, 66, 48, 69, 26, 48, 23, 24, 19, 7
         };
 
-        private int target=100;
+        private int target = 171;
 
         public ThreeNumbersInTheArray ( )
         {
-            SearchForThreeTermsBasedOnTheirAmount( array , target );
+            var l = SearchForThreeTermsBasedOnTheirAmount( array , target );
+            int count = 1;
+            foreach ( var val in l )
+            {
+                Console.WriteLine( $"{count}   {val.Item1} + {val.Item2} + {val.Item3} = {val.Item1 + val.Item2 + val.Item3}" );
+                count++;
+            }
         }
 
         private List<Tuple<int , int , int>> SearchForThreeTermsBasedOnTheirAmount ( int[ ] array , int target )
         {
             List<Tuple<int , int , int>> list = new List<Tuple<int , int , int>>( array.Length / 3 );
-            //SortedSet<int> set = new SortedSet<int>( array.Where( x => x < target ) );
             var val1 = new List<int>( array.Where( x => x < target ) );
-            int v1, v2, v3 = 0;
-            if ( val1.Count() > 2 )
+            int v1, v2, v3, difference = 0;
+            if ( val1.Count( ) > 2 )
             {
-                for(int i=0 ; i < val1.Count ; i++ )
+                for ( int i = 0 ; i < val1.Count ; i++ )
                 {
                     v1 = val1[ i ];
-                    var val2 = val1.Where( x => x < target - v1 );
-                    if ( val2.Count() > 2 )
+                    difference = target - v1;
+                    var val2 = val1.Where( x => x < difference && !x.Equals( v1 ) ).ToList<int>( );
+                    if ( val2.Count( ) > 1 )
                     {
-
+                        for ( int j = 0 ; j < val2.Count( ) ; j++ )
+                        {
+                            v2 = val2[ j ];
+                            difference = target - v1 - v2;
+                            var val3 = val2.Where( x => x <= difference && !x.Equals( v2 ) ).ToList<int>( );
+                            v3 = val3.Where( x => x == difference ).FirstOrDefault( );
+                            if ( v1 + v2 + v3 == target && !list.Any( x => x.Item1 == v1 && x.Item2 == v2 && 
+                            x.Item3 == v3 ) )
+                            {
+                                list.Add( new Tuple<int , int , int>( v1 , v2 , v3 ) );
+                                val1.Remove( v1 );
+                                val1.Remove( v2 );
+                                val1.Remove( v3 );
+                            }
+                        }
                     }
                 }
             }
-
             return list;
         }
 
