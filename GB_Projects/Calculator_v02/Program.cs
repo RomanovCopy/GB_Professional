43efbe99a0314a39306539400e79143363ca1c22
RomanovCopy <RomanovCopy@gmail.com>
2024-03-02 09:06:39 +0300

    Start

---
+++ GB_Projects/Calculator_v02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator_v02
+{
+    internal class Program
+    {
+        static void Main ( string[ ] args )
+        {
+            Console.WriteLine( "Hello, World!" );
+        }
+    }
+}
