1800a36b0bbd8ec7cc850f24d3c816fa8c22485c
RomanovCopy <RomanovCopy@gmail.com>
2024-03-04 19:55:42 +0300

    Start

---
+++ GB_Projects/Lesson06_DelegatesAndEvents/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson06_DelegatesAndEvents
+{
+    internal class Program
+    {
+        static void Main ( string[ ] args )
+        {
+            Console.WriteLine( "Hello, World!" );
+        }
+    }
+}
