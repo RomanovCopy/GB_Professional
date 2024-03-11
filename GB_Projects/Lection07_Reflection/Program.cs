2ae20518fa3ecac67f36741bda4b28f7a50aebd4
RomanovCopy <RomanovCopy@gmail.com>
2024-03-11 19:53:57 +0300

    Start

---
+++ GB_Projects/Lection07_Reflection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lection07_Reflection
+{
+    internal class Program
+    {
+        static void Main ( string[ ] args )
+        {
+            Console.WriteLine( "Hello, World!" );
+        }
+    }
+}
