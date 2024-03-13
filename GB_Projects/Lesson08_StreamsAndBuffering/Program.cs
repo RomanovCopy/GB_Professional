c92dce55d4b630f8b5a90d60d588b5d94642997d
RomanovCopy <RomanovCopy@gmail.com>
2024-03-13 18:58:13 +0300

    Start

---
+++ GB_Projects/Lesson08_StreamsAndBuffering/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson08_StreamsAndBuffering
+{
+    internal class Program
+    {
+        static void Main ( string[ ] args )
+        {
+            Console.WriteLine( "Hello, World!" );
+        }
+    }
+}
